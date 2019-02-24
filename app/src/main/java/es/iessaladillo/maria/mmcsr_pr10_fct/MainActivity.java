88b97f1b9a5acfed11ca22e7872ecc4e6e2d0a44
Maria <mariamunozcruzado@gmail.com>
2019-02-24 17:11:46 +0100

    Initial commit

---
+++ app/src/main/java/es/iessaladillo/maria/mmcsr_pr10_fct/MainActivity.java
@@ -0,0 +1,14 @@
+package es.iessaladillo.maria.mmcsr_pr10_fct;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
